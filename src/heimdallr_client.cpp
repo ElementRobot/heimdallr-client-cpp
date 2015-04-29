35717e3cbe458fc71f7ad1fbab39e5c614fb6442
jf <jf@elementrobot.com>
2015-04-28 18:43:29 -0700

    Ready to start writing tests.

--- src/heimdallr_client.cpp
+++ src/heimdallr_client.cpp
@@ -1,20 +1,109 @@
-#include "heimdallr_client.h"
 
 #include <cstdlib>
 #include <stdio.h>
+#include <exception>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include "heimdallr_client.h"
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+string now(void) {
+    using namespace boost::posix_time;
+    ptime t = microsec_clock::universal_time();
+    return to_iso_extended_string(t) + "Z";
+}
 
-    sio::client manager;
-    // manager.connect("http://hmdlr.co");
-
-    printf("Oh YEAH!\n");
-
-    return 0;
-} 
- 
+namespace hmdlr {
+    Client::Client(string token, string nsp) {
+        ready_ = false;
+        token_ = token;
+        connection_ = client_.socket(nsp);
+        
+        connection_->on("hmdlr-error", [&](string const& name, sio::message::ptr const& data, bool is_ack, sio::message::ptr& ack_resp) {
+            string msg = data->get_map()["message"]->get_string();
+            
+            throw error(msg.c_str());
+        });
+        
+        connection_->on("auth-success", [&](sio::event& _) {
+            ready_ = true;
+            
+            for (unsigned i = 0; i < delayed_messages_.size(); i++) {
+                connection_->emit(
+                    delayed_messages_[i].name,
+                    delayed_messages_[i].packet
+                );
+            }
+            delayed_messages_.clear();
+        });
+        
+        client_.set_open_listener([&]() {
+            sio::message::ptr packet = sio::object_message::create();
+            
+            packet->get_map()["token"] = sio::string_message::create(token_);
+            packet->get_map()["authSource"] =
+                sio::string_message::create(auth_source_);
+            
+            connection_->emit("authorize", packet);
+        });
+    }
+    
+    void Client::connect() {
+        client_.connect(url_);
+    }
+    
+    void Client::on(string msg_name, sio::socket::event_listener fn) {
+        connection_->on(msg_name, fn);
+    }
+    
+    void Client::sendMsg(string msg_name, sio::message::ptr const& packet) {
+        if (ready_) {
+            printf("SEDNING PACKET");
+            connection_->emit(msg_name, packet);
+        } else {
+            printf("DELAYED PACKET");
+            delayed_messages_.push_back(delayed_message(msg_name, packet));
+        }
+    }
+    
+    Provider::Provider(string token) : Client(token, "/provider") {}
+    
+    void Provider::sendEvent(string subtype, sio::message::ptr const& data) {
+        sio::message::ptr packet = sio::object_message::create();
+        
+        packet->get_map()["subtype"] = sio::string_message::create(subtype);
+        packet->get_map()["data"] = data;
+        packet->get_map()["t"] = sio::string_message::create(now());
+        
+        sendMsg("event", packet);
+    }
+    
+    void Provider::sendSensor(string subtype, sio::message::ptr const& data) {
+        sio::message::ptr packet = sio::object_message::create();
+        
+        packet->get_map()["subtype"] = sio::string_message::create(subtype);
+        packet->get_map()["data"] = data;
+        packet->get_map()["t"] = sio::string_message::create(now());
+        
+        sendMsg("sensor", packet);
+    }
+    
+    void Provider::completed(string uuid) {
+        sio::message::ptr packet = sio::object_message::create();
+        
+        packet->get_map()["subtype"] = sio::string_message::create("completed");
+        packet->get_map()["uuid"] = sio::string_message::create(uuid);
+        packet->get_map()["t"] = sio::string_message::create(now());
+        
+        sendMsg("event", packet);
+    }
+    
+    Consumer::Consumer(string token) : Client(token, "/consumer") {}
+    
+    void Consumer::getState(string uuid, vector<string> subtypes) { throw "Not Implemented"; };
+    void Consumer::joinStream(string uuid) { throw "Not Implemented"; };
+    void Consumer::leaveStream(string uuid) { throw "Not Implemented"; };
+    void Consumer::setFilter(string uuid, sio::message::ptr const& data) { throw "Not Implemented"; };
+    void Consumer::sendControl(string uuid, sio::message::ptr const& data) { throw "Not Implemented"; };
+}
--- src/heimdallr_client.h
+++ src/heimdallr_client.h
@@ -1,4 +1,81 @@
 
+#include <vector>
+
 #include "sio_client.h"
 #include "sio_message.h"
 #include "sio_socket.h"
+
+#ifndef HEIMDALLR_CLIENT
+#define HEIMDALLR_CLIENT
+
+using namespace std;
+
+string now(void);
+
+
+namespace hmdlr {
+    class error : public exception {
+    public:
+        explicit error(const char* message) : msg_(message) {}
+        explicit error(const string& message) : msg_(message) {}
+        virtual ~error() throw () {}
+        virtual const char* what() const throw () {
+            return msg_.c_str();
+        }
+    protected:
+        string msg_;
+    };
+    
+    struct delayed_message {
+        string name;
+        sio::message::ptr packet;
+        delayed_message(string name, sio::message::ptr packet) : 
+            name(name),
+            packet(packet)
+        {};
+    };
+    
+    class Client {
+    public:
+       Client(string, string);
+        void connect(void);
+        void on(string, sio::socket::event_listener);
+        void removeListener(string);
+        void removeListener(string, sio::socket::event_listener);
+      
+    protected:
+        void sendMsg(string, sio::message::ptr const&);
+        string url_ = "https://heimdallr.co";
+        string auth_source_ = "skyforge";
+        
+    private:
+        string token_;
+        bool ready_;
+        sio::client client_;
+        sio::socket::ptr connection_;
+        vector<delayed_message> delayed_messages_;
+      
+    };
+    
+    class Provider : public Client {
+    public:
+        Provider(string);
+        void completed(string);
+        void sendEvent(string, sio::message::ptr const&);
+        void sendSensor(string, sio::message::ptr const&);
+    };
+    
+    class Consumer : public Client {
+    public:
+        Consumer(string);
+        void getState(string, vector<string>);
+        void joinStream(string);
+        void leaveStream(string);
+        void setFilter(string, sio::message::ptr const&);
+        void sendControl(string, sio::message::ptr const&);
+        
+    };
+}
+
+
+#endif
